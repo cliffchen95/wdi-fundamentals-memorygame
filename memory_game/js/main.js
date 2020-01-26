57ed5413819973c4f62e33f0e7577e088fbb74a8
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-01-26 15:28:51 -0500

    updates

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running");
+let cardOne = "queen";
+let cardTwo = cardOne;
+let cardThree = "king";
+let cardFour = cardThree;
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
