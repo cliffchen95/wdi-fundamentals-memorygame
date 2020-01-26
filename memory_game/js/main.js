92a831d7985c0ba2e0c06363a2042e5d46435fae
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-01-26 16:15:01 -0500

    Update main.js with functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,19 @@
 let cards = ["queen", "queen", "king", "king"]
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
 
-if (cardsInPlay.length === 2) {
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+};
+
+function checkForMatch () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match");
 	} else {
 		alert("Sorry, try again");
 	}
 }
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
