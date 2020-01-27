5a90b59adf1cc2c2c0a9b3be1c0557ec1450e099
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-01-27 16:52:03 -0500

    update functions
    new functions: create board, reset and flipcard
    new object in html: score, button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,41 +2,64 @@ let cards = [
 {
 rank:"queen",
 suit: "hearts",
-cardImage: "image/queen-of-hearts.jpg"
+cardImage: "images/queen-of-hearts.png"
 },
 {
 rank:"queen",
 suit: "diamonds",
-cardImage: "image/queen-of-diamonds.jpg"
+cardImage: "images/queen-of-diamonds.png"
 },
 {
 rank:"king",
 suit: "hearts",
-cardImage: "image/king-of-hearts.jpg"
+cardImage: "images/king-of-hearts.png"
 },
 {
 rank:"king",
 suit: "hearts",
-cardImage: "image/king-of-diamonds.jpg"
+cardImage: "images/king-of-diamonds.png"
 }
 ]
 let cardsInPlay = [];
+let score = 0;
 
-function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+function flipCard() {
+	let cardId = this.getAttribute('id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-	console.log("The suit: " + cards[cardId].suit);
-	console.log("The image: " + cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 };
 
 function checkForMatch () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match");
+		score ++;
+		document.getElementById("score").textContent = "You've Found " + score + " Matches!";
 	} else {
 		alert("Sorry, try again");
 	}
 }
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('id', i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
+
+function reset() {
+ 	for (let i = 0; i < cards.length; i++) {
+ 		let cardElement = document.getElementById(i);
+ 		cardElement.setAttribute('src', "images/back.png");
+ 	}
+ 	cardsInPlay = [];
+}
+
+document.getElementById('reset').addEventListener('click', reset);
